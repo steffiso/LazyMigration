272a66772c7892589ce4dbd364c2da51b674dbb0
Steffi <steffi.sombach@t-online.de>
2015-12-03 20:38:20 +0100

    copy + move erweitert, funktioniert flexibler (siehe Testaufrufe in Main.java)

--- src/lazyMigration/Main.java
+++ src/lazyMigration/Main.java
@@ -12,12 +12,13 @@ public class Main {
 		//String datalogRules = edb.getAll("Mission");
 		//String datalogRules = edb.get("Mission",3);
 		//String datalogRules = edb.getEDBFacts();
-		//String datalogRules = edb.addAttribute("Mission", "priority", "2");
-		//String datalogRules = edb.deleteAttribute("Player", "name");
+		//String datalogRules = edb.addAttribute("Mission", "priority", "200");
+		//String datalogRules = edb.deleteAttribute("Mission", "title");
 
-		//!!! move/copyAttribute funktioniert aktuell nur für where player.id = mission.pid !!!
-		String datalogRules = edb.copyAttribute("Player", "Mission", "score", "id", "pid");		
+		//String datalogRules = edb.copyAttribute("Player", "Mission", "score", "id", "pid");		
+		//String datalogRules = edb.copyAttribute("Mission", "Player", "title", "pid", "id");		
 		//String datalogRules = edb.moveAttribute("Player", "Mission", "score", "id", "pid");
+		String datalogRules = edb.moveAttribute("Mission", "Player", "title", "pid", "id");
 		System.out.println(datalogRules);
 
 	}
